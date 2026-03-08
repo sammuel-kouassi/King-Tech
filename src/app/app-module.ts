45250492da2219edeccbc031a4a8106af8d3b381
sammuel-kouassi <samuuel.kouassi2026@gmail.com>
2026-03-08 03:01:41 +0000

    first

---
+++ src/app/app-module.ts
@@ -0,0 +1,53 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Navbar } from './navbar/navbar';
+import { HeroComponent } from './hero.component/hero.component';
+import { FeaturesBarComponent } from './features-bar.component/features-bar.component';
+import { PopularProductsComponent } from './popular-products.component/popular-products.component';
+import { CategoriesComponent } from './categories.component/categories.component';
+import { NewsletterComponent } from './newsletter.component/newsletter.component';
+import { FooterComponent } from './footer.component/footer.component';
+import { HomeComponent } from './home.component/home.component';
+import { FormationComponent } from './formation.component/formation.component';
+import { CartComponent } from './cart.component/cart.component';
+import { AProposComponent } from './pages/a-propos.component/a-propos.component';
+import { RetoursComponent } from './pages/retours.component/retours.component';
+import { CheckoutComponent } from './checkout.component/checkout.component';
+import { SuccessComponent } from './success.component/success.component';
+import { Partenaire } from './partenaire/partenaire';
+
+@NgModule({
+  declarations: [
+    App,
+    Navbar,
+    HeroComponent,
+    FeaturesBarComponent,
+    PopularProductsComponent,
+    CategoriesComponent,
+    FooterComponent,
+    HomeComponent,
+    FormationComponent,
+    AProposComponent,
+    RetoursComponent,
+    Partenaire
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+    NewsletterComponent,
+    CartComponent,
+    CheckoutComponent,
+    SuccessComponent,
+    NgOptimizedImage
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
---
+++ src/app/app-routing-module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component/home.component';
+import { BoutiqueComponent } from './boutique.component/boutique.component';
+import {ProductDetailComponent} from './product-detail.component/product-detail.component';
+import { CommunauteComponent } from './communaute.component/communaute.component';
+import { FormationComponent } from './formation.component/formation.component';
+import {AProposComponent} from './pages/a-propos.component/a-propos.component';
+import {RetoursComponent} from './pages/retours.component/retours.component';
+import {CheckoutComponent} from './checkout.component/checkout.component';
+import {SuccessComponent} from './success.component/success.component';
+
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'boutique', component: BoutiqueComponent },
+  { path: 'produit/:id', component: ProductDetailComponent },
+  { path: 'communaute', component: CommunauteComponent },
+  { path: 'formation', component: FormationComponent },
+  { path: 'a-propos', component: AProposComponent },
+  { path: 'retours', component: RetoursComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'success', component: SuccessComponent },
+  { path: '**', redirectTo: '' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
